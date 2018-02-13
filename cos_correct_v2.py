882efbf91430ef7853669f78776955bea442fc99
chadwickcasp <casperchadwick@gmail.com>
2018-02-13 15:20:03 -0800

    Broken

--- cos_correct_v2.py
+++ cos_correct_v2.py
@@ -7,7 +7,7 @@ import datetime as dt
 import math
 import numpy as np
 import requests
-from requests.exceptions import ConnectionError
+from requests.exceptions import ConnectionError, RequestException
 import pandas as pd
 import sys
 if sys.version_info[0] < 3: 
@@ -17,6 +17,7 @@ else:
 import csv
 import socket
 import ast
+import time
 
 # Config Settings
 latitude = math.radians(37.595932)
@@ -56,67 +57,17 @@ if __name__ == '__main__':
                'endDate': nowDateString, 
                'fileName':str(nowDateFileGETString)+'.csv'}
 
-
-    # Search for RaZON+
-    # mac = [22, 445, 548, 631]
-    # linux = [20, 21, 22, 23, 25, 80, 111, 443, 445, 631, 993, 995]
-    # windows = [135, 137, 138, 139, 445]
-    # aios = [49152, 62078] # Apple iOS (ios is also the name for Cisco's OS running on their products)
-
-    # import subprocess
-    # import sys
-     
-    # ip = '192.168.15.112'
-        
-    # # ping ip
-    # p = subprocess.Popen(['ping', ip], stdout=subprocess.PIPE,
-    #         stderr=subprocess.PIPE)
-     
-    # out, err = p.communicate()
-     
-    # # arp list
-    # p = subprocess.Popen(['arp', '-n', ip], stdout=subprocess.PIPE,
-    #         stderr=subprocess.PIPE)
-     
-    # out, err = p.communicate()
-     
-    # try:
-    #     arp = [x for x in out.split('\n') if ip in x][0]
-    #     print(arp)
-    # except IndexError:
-    #     print("Error")
-    #     sys.exit(1)     # no arp entry found
-
-
-    # socket_obj = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-    # socket.setdefaulttimeout(1)
-    # possible_connects = []
-    # result = socket_obj.connect_ex(('192.168.15.112',22))
-    # print(result, socket.getfqdn('192.168.15.112'), socket.gethostbyaddr("192.168.15.112"), socket_obj.getsockname(), socket.gethostname())
-
-    # for addr in ['192.168.15.'+str(i) for i in range(0, 255)]:
-    #     try:
-    #         result = socket_obj.connect_ex((addr,22))
-    #         if result == 0:
-    #             possible_connects.append(addr)
-    #     except:
-    #         print("Error")
-    # socket_obj.close()
-    # print(possible_connects)
-    # sys.exit(0)
-
-
     # Make request to RaZON for data, handle connection error
     try:
         r5 = requests.get("http://"+str(razonIP)+"/loggings/exportdata.csv", data=payload)
         # print('Solar angle request: {}'.format(r5.content))
-    except ConnectionError:
-        raise
+    except RequestException as e:
+        raise e
     try:
         r6 = requests.get("http://"+str(razonIP)+"/status_trackings/lastirradiance?")
         # print('Last Irradiance request: {}'.format(r6.content))
-    except ConnectionError:
-        raise
+    except RequestException as e:
+        raise e
 
     # Convert into readable csv and data
     sio = StringIO(r5.content)
@@ -146,7 +97,7 @@ if __name__ == '__main__':
                         hour=startHours, 
                         minute=startMinutes, 
                         second=now.second)
-    print(start)
+    # print(start)
     greaterThanStart = (pd.to_datetime(times, format=' %H:%M:%S') > pd.to_datetime(start))
     end = dt.datetime(year=1900, 
                       month=1, 
@@ -155,13 +106,14 @@ if __name__ == '__main__':
                       minute=now.minute, 
                       second=now.second)
     lessThanEnd = (pd.to_datetime(times, format=' %H:%M:%S') <= pd.to_datetime(end))
-    print(end)
+    # print(end)
+    time.sleep(5)
     dni_df = dni_df[greaterThanStart & lessThanEnd]
     dni_df = dni_df.reset_index()
     # print(dni_df)
     # print(len(dni_df.index))
-    if len(dni_df.index) == 0:
-        sys.exit(0)
+    # if len(dni_df.index) == 0:
+    #     sys.exit(0)
 
     # Solar angles used for cosine correction
     azimuth_angles = (dni_df['SolarAzimuth (Degrees)']).map(math.radians)
@@ -181,14 +133,12 @@ if __name__ == '__main__':
     illumination = dni_df['Irrid. (W/m2)']
     dni_df['Cosine Corrected DNI'] = illumination*(dni_df['Cos(Theta)']*
                                                    dni_df['Cos(Phi)'])
-    # print(dni_df['Cos(Theta)'])
-    # print(dni_df['Cos(Phi)'])
-    irrid = irrid*float(dni_df['Cos(Theta)'])*float(dni_df['Cos(Phi)'])
 
-    # print(dt.datetime.strptime(dni_df['Time (hh:mm:ss)'].ix[0], ' %H:%M:%S'), 
-    #       float(dni_df['Cosine Corrected DNI']))
-    # print(dni_df)
-    print(float(dni_df['Theta_']), 
-          float(dni_df['Phi_']),
-          dni_df['Time (hh:mm:ss)'].ix[0][1:], 
-          float(irrid))
+    try:
+        irrid = irrid*float(dni_df['Cos(Theta)'])*float(dni_df['Cos(Phi)'])
+        print(float(dni_df['Theta_']), 
+              float(dni_df['Phi_']),
+              dni_df['Time (hh:mm:ss)'].ix[0][1:], 
+              float(irrid))
+    except TypeError as e:
+        print("RaZON data not here yet...")
